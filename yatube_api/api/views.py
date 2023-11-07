b55f92b8f22646d08c8099a49095fa158ed7d9eb
ruki-qq <ruki@duck.com>
2023-11-07 21:35:26 +0300

    Delete junk

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import IsAuthenticated
 
 from .permissions import IsAuthorOrReadOnly
 from .serializers import CommentSerializer, GroupSerializer, PostSerializer
-from posts.models import Comment, Group, Post
+from posts.models import Group, Post
 
 
 class PostModelViewSet(viewsets.ModelViewSet):
